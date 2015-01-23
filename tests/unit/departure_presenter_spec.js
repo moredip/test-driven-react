195fde271a817c9e7dfa8781d10e3bf6fa4c0c27
Pete Hodgson <git@thepete.net>
2015-01-23 13:39:14 -0800

    completed tests commented out

--- tests/unit/departure_presenter_spec.js
+++ tests/unit/departure_presenter_spec.js
@@ -20,46 +20,74 @@ describe( 'Departure presenter', function(){
     expect(presentation).to.have.property('destination','Ontario');
   });
   
-  it('presents the departure time for "10 mins"', function(){
-    // Given
-    var departure = {
-      dest_name: "blah",
-      etd: 10
-    };
-    
-    // When
-    var presentation = departurePresenter(departure);
-    
-    // Then
-    expect(presentation).to.have.property('departing','10 mins');
-  });
-  
-  it('presents the departure time for "1 min"', function(){
-    // Given
-    var departure = {
-      dest_name: "blah",
-      etd: 1
-    };
-    
-    // When
-    var presentation = departurePresenter(departure);
-    
-    // Then
-    expect(presentation).to.have.property('departing','1 min');
-  });
 
-  it('presents the departure time for "now"', function(){
-    // Given
-    var departure = {
-      dest_name: "blah",
-      etd: 0
-    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //it('presents the departure time for "10 mins"', function(){
+    //// Given
+    //var departure = {
+      //dest_name: "blah",
+      //etd: 10
+    //};
     
-    // When
-    var presentation = departurePresenter(departure);
+    //// When
+    //var presentation = departurePresenter(departure);
     
-    // Then
-    expect(presentation).to.have.property('departing','now');
-  });
+    //// Then
+    //expect(presentation).to.have.property('departing','10 mins');
+  //});
+  
+  //it('presents the departure time for "1 min"', function(){
+    //// Given
+    //var departure = {
+      //dest_name: "blah",
+      //etd: 1
+    //};
+    
+    //// When
+    //var presentation = departurePresenter(departure);
+    
+    //// Then
+    //expect(presentation).to.have.property('departing','1 min');
+  //});
+
+  //it('presents the departure time for "now"', function(){
+    //// Given
+    //var departure = {
+      //dest_name: "blah",
+      //etd: 0
+    //};
+    
+    //// When
+    //var presentation = departurePresenter(departure);
+    
+    //// Then
+    //expect(presentation).to.have.property('departing','now');
+  //});
 
 });
